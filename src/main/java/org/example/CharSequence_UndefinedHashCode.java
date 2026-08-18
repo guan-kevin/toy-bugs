a52880754cf9589b02f48d2fef73df4d15a717af
guan-kevin <20823498+guan-kevin@users.noreply.github.com>
2026-08-18 15:49:12 -0400

    refactor: change Segment type to CharSequence in foo and bar methods

--- src/main/java/org/example/CharSequence_UndefinedHashCode.java
+++ src/main/java/org/example/CharSequence_UndefinedHashCode.java
@@ -4,12 +4,12 @@ import javax.swing.text.Segment;
 
 public class CharSequence_UndefinedHashCode {
     public void foo() {
-        Segment segment = new Segment();
+        CharSequence segment = new Segment();
         segment.hashCode();
     }
 
     public void bar() {
-        Segment segment = new Segment();
+        CharSequence segment = new Segment();
         segment.equals(new Segment());
     }
 }
