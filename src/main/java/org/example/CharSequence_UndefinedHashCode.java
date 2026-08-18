be453056e891d317142a89f2e364442d04dd0963
guan-kevin <20823498+guan-kevin@users.noreply.github.com>
2026-08-18 15:54:32 -0400

    more test

--- src/main/java/org/example/CharSequence_UndefinedHashCode.java
+++ src/main/java/org/example/CharSequence_UndefinedHashCode.java
@@ -4,12 +4,40 @@ import javax.swing.text.Segment;
 
 public class CharSequence_UndefinedHashCode {
     public void foo() {
-        CharSequence segment = new Segment();
+        CharSequence segment = new MyCharSequence();
         segment.hashCode();
     }
 
     public void bar() {
-        CharSequence segment = new Segment();
-        segment.equals(new Segment());
+        CharSequence segment = new MyCharSequence();
+        segment.equals(new MyCharSequence());
+    }
+
+    public class MyCharSequence implements CharSequence {
+
+        @Override
+        public int hashCode() {
+            return 42;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return this == obj;
+        }
+
+        @Override
+        public int length() {
+            return 0;
+        }
+
+        @Override
+        public char charAt(int index) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        @Override
+        public CharSequence subSequence(int start, int end) {
+            return this;
+        }
     }
 }
