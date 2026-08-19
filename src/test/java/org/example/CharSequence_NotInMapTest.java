f3058957be333e230ef05866c162f2ddf4776ea0
guan-kevin <20823498+guan-kevin@users.noreply.github.com>
2026-08-18 22:12:45 -0400

    add

--- src/test/java/org/example/CharSequence_NotInMapTest.java
+++ src/test/java/org/example/CharSequence_NotInMapTest.java
@@ -10,5 +10,8 @@ public class CharSequence_NotInMapTest {
     @Test public void testNotInMap() {
         Map<CharSequence, String> map = new HashMap<>();
         map.put((new CharSequence_UndefinedHashCode()).new MyCharSequence(), "value");
+
+        Map<CharSequence, String> map2 = new HashMap<>();
+        map2.putAll(map);
     }
 }
