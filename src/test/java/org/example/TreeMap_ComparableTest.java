a3500ceaf81de6052e63bf84dee8791fc2aeacce
guan-kevin <20823498+guan-kevin@users.noreply.github.com>
2026-08-20 20:23:43 -0400

    add more true bugs

--- src/test/java/org/example/TreeMap_ComparableTest.java
+++ src/test/java/org/example/TreeMap_ComparableTest.java
@@ -9,12 +9,20 @@ public class TreeMap_ComparableTest {
         TreeMap<SortedSet_Comparable, String> map = new TreeMap<>();
         SortedSet_Comparable obj = new SortedSet_Comparable();
         map.put(obj, "value");
+    }
 
+    @Test public void testTreeMap2() {
+        SortedSet_Comparable obj = new SortedSet_Comparable();
         Map<SortedSet_Comparable, String> map2 = new HashMap<>();
         map2.put(obj, "value");
 
         TreeMap<SortedSet_Comparable, String> map3 = new TreeMap<>(map2);
+    }
 
+    @Test public void testTreeMap3() {
+        SortedSet_Comparable obj = new SortedSet_Comparable();
+        Map<SortedSet_Comparable, String> map2 = new HashMap<>();
+        map2.put(obj, "value");
         TreeMap<SortedSet_Comparable, String> map4 = new TreeMap<>();
         map4.putAll(map2);
     }
